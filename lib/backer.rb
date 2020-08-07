af028fba2337347b6e8f2966220f37d50133cd67
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-08-07 16:41:00 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,6 +12,7 @@ class Backer
   def back_project(project)
     @backed_projects << project
     project.backers << self unless project.backers.include?(self)
+      # adds self to @backers array in Project class without creating duplicates
   end
   
 end
